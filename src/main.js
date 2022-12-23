3aeef77749dbfa584c58999fc08ecd5cd4b4553f
Jose A. Garcia Villanueva <jagarcia@grupomavi.com>
2022-12-23 21:53:38 +0100

    Commit proyecto finalizado

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify'
 
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
+
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    loading: {
+      titulo: '',
+      estado: false,
+      color:'blue'
+    }
   },
   getters: {
   },
   mutations: {
+    mostrarLoading(state, payload) {
+      state.loading.titulo = payload.titulo
+      state.loading.color = payload.color
+      state.loading.estado = true
+      setTimeout(() => {
+        
+      }, 10000);
+    },
+
+    ocultarLoading(state) {
+      state.loading.estado = false
+    }
   },
   actions: {
   },
